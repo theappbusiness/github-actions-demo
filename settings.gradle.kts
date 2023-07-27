8bba1d3d07b39ca544d366c4169c8bb5566e432c
shavinda <shavinda.rabichandran@kinandcarta.com>
2023-07-27 18:15:54 +0100

    Initial workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,22 @@ dependencyResolutionManagement {
 
 rootProject.name = "GithubActionsDemo"
 include(":app")
+
+/*
+    Setup to allow build scans to be published
+    If builds scans are not needed this is not required.
+ */
+plugins {
+    id("com.gradle.enterprise") version "3.14.1"
+}
+
+gradleEnterprise {
+    // configuration
+    buildScan {
+        if (System.getenv("CI_BUILD").orEmpty().equals("true", ignoreCase = true)) {
+            // Connecting to scans.gradle.com by agreeing to the terms of service
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
